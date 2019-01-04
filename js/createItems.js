85ffe9b7fd1186d1e16dc2d672e29d9ceda79e01
Liam <liamallen792@gmail.com>
2019-01-04 12:20:09 -0800

    basic editor functionality done

--- js/createItems.js
+++ js/createItems.js
@@ -1,37 +1,8 @@
 //setting up variables
 const container=document.querySelector(`.store`);
 const footer=document.querySelector(`.footer`);
-//input variables - only applicable to editor
-const nameInput=document.querySelector(`#name`);
-const enter=document.querySelector(`button`);
-const descInput=document.querySelector(`#desc`);
-const thumbInput=document.querySelector(`#thumb`);
 
-var itemArray=[];
-var savedArray=undefined;
 
-function editor(){
-  //editor form event listeners
-  enter.addEventListener(`click`, function(event){
-    //when enter button is clicked
-    itemArray.push({
-      //push these things to an array in the form of one object
-      name:nameInput.value,
-      desc:descInput.value,
-      image:thumbInput.value.split("\\")[thumbInput.value.split("\\").length-1]
-    });
-    //reset values of inputs
-    nameInput.value="";
-    descInput.value="";
-    thumbInput.value="";
-
-    createStore(itemArray);
-  })
-  save.addEventListener(`click`, function(event){
-    saveStore(itemArray);
-  })
-}
-editor();
 //turning the itemArray[] array into HTML
 function createStore(array){
   container.innerHTML="";
@@ -51,7 +22,7 @@ function createStore(array){
       <!-- the thumbnail image -->
       <img class="item-thumbnail img-fluid" src="../images/${array[i].image}">
     </div>
-    <div class="col-md-8 store-text-contianer">
+    <div class="col-md-8 store-text-container">
       <!-- the name of the item -->
       <h3>
         <a class="store-link" href="store-items/${array[i].name}.html">${array[i].name}</a>
@@ -64,11 +35,4 @@ function createStore(array){
   return JSON.stringify(array);
 }
 
-//turning the itemArray into JSON
-function saveStore(array){
-  if(itemArray[0]!=undefined){
-    savedArray=JSON.stringify(itemArray);
-  }
-}
-
-export {createStore, savedArray};
+export {createStore};
--- js/store.js
+++ js/store.js
@@ -1,11 +1,32 @@
-import {createStore, savedArray} from "/js/createItems.js";
+import {createStore} from "/js/createItems.js";
+//setting up necesarry variables
+var savedArray=[];
+var timer=""
+var obj={};
+//loads list from storeContent.json, not run until later
+function loadJson(callback){
+  var xobj=new XMLHttpRequest();
+  xobj.overrideMimeType(`application/json`);
+  xobj.open(`GET`, `../pages/store-content/storeContent.json`, false);
+  xobj.onreadystatechange=function(){
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
 
-const save=document.querySelector(`#save`);
+function init() {
+  loadJson(function(response) {
+  // Parse JSON string into object
+    savedArray=JSON.parse(response).savedStore;
+    createStore(savedArray);
+  });
+}
 
+init();
 
-save.addEventListener(`click`, function(event){
-  if(savedArray){
-    const staticStoreArray=JSON.parse(savedArray);
-    createStore(staticStoreArray);
-  }
-})
+console.log(savedArray);
+
+export {savedArray};
